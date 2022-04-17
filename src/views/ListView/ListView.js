6f084909c42cc523e20e31a21333f25213a347e1
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2022-04-17 23:04:09 +0300

    removeLink bug fix

--- src/views/ListView/ListView.js
+++ src/views/ListView/ListView.js
@@ -80,7 +80,7 @@ const ListView = ({ itemCount, changeView }) => {
 			sortTypes.mostVotes
 		);
 
-		if (data.length !== 1 && data.length % itemCount === 1) {
+		if (data.length !== 1 && data.length % itemCount === 1 && data.currentPage === pageCount) {
 			setCurrentPage((currentPage) => currentPage - 1);
 		}
 
